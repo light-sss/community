9af23d18f1c3a182f1745efed9e1556e9dc4dc48
light <3237359569@qq.com>
2023-05-22 17:13:52 +0800

    发送、显示系统消息

--- src/main/java/com/light/community/config/WebMvcConfig.java
+++ src/main/java/com/light/community/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.light.community.config;
 
 import com.light.community.controller.interceptor.LoginTicketInterceptor;
 import com.light.community.controller.interceptor.LoginRequiredInterceptor;
+import com.light.community.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -22,6 +23,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(loginTicketInterceptor)
                 .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.jpeg","/**/*.png");
@@ -29,5 +33,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginRequiredInterceptor)
                 .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.jpeg","/**/*.png");
 
+        registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.jpeg","/**/*.png");
     }
 }
